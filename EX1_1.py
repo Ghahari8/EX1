c599648b23821048e1af85d8e4234fadd22300d9
Ghahari8 <116120808+Ghahari8@users.noreply.github.com>
2022-10-26 07:30:29 +0330

    Add files via upload

---
+++ EX1_1.py
@@ -0,0 +1,17 @@
+def Ex1_1(next_number):
+ '''
+ (int) -> int
+
+ Adds 1 to the variable.
+ example:
+ >>> Ex1_example(5)
+ 6
+
+ '''
+ next_number=number+1
+ number=int(input("your name :"))
+ return next_number
+
+
+
+
